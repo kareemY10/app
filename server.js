feb294326148d6d2f2b51f1a06b60f0c69896971
kareemY10 <kareem.skr@gmail.com>
2023-06-25 21:45:55 +0300

    starting Template

---
+++ server.js
@@ -0,0 +1,26 @@
+
+
+const express=require('express')
+const app=express()
+const server=require('http').Server(app)
+const hostname = '127.0.0.1';
+const port = 3000;
+
+
+
+app.set('view engine','ejs')
+app.use(express.static('public'))
+
+
+app.get('/', (req,res)=>{
+    res.render('main',{roomId:req.params.room})
+})
+
+
+
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
+
+
